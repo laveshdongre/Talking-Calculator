582d0d457013fd14387c48d758d72458df3f40cc
Lavesh Dongre <85252596+laveshdongre@users.noreply.github.com>
2025-04-14 15:43:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let displayValue = "";
+
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById("display").value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = "";
+    document.getElementById("display").value = "";
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue);
+        document.getElementById("display").value = displayValue;
+    } catch (error) {
+        document.getElementById("display").value = "Error";
+    }
+}
+
+function speakResult() {
+    const msg = new SpeechSynthesisUtterance(displayValue.toString());
+    window.speechSynthesis.speak(msg);
+}
